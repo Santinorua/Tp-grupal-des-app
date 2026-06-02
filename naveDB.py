02fe94aa97368fa1e6d98edbc94a3b09c5144885
Santinorua <santinoruani@gmail.com>
2026-06-02 09:00:50 -0300

    Nueva conexión con db nave

--- naveDB.py
+++ naveDB.py
@@ -1,27 +1,49 @@
 import sqlite3
+from g4b import Database
 
 class NaveDB:
     def __init__(self):
-        self.conexion = sqlite3.connect('agencia_espacial.db')
-        self.cursor = self.conexion.cursor()
+        self.db = Database()
+        
 
     def query_create_nave(self, nave):
-        self.cursor.execute('INSERT INTO naves (nombre_nave, modelo, capacidad_pasajeros) VALUES (?, ?, ?)', (nave.nombre_nave, nave.modelo, nave.capacidad_pasajeros))
-        self.conexion.commit()
+        conn = self.db.conectar()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO naves (nombre_nave, modelo, capacidad_pasajeros) VALUES (?, ?, ?)', (nave.nombre_nave, nave.modelo, nave.capacidad_pasajeros))
+        nave.id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+        return nave.id
 
     def query_read_all_naves(self):
-        self.cursor.execute('SELECT * FROM naves')
-        return self.cursor.fetchall()
+        conn = self.db.conectar()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM naves')
+        data = cursor.fetchall()
+        conn.commit()
+        conn.close()
+        return data
 
     def query_read_one_nave(self, id):
-        self.cursor.execute('SELECT * FROM naves WHERE id = ?', (id))
-        return self.cursor.fetchone()
+        conn = self.db.conectar()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM naves WHERE id = ?', (id))
+        data = cursor.fetchone()
+        conn.commit()
+        conn.close()
+        return data
 
     def query_update_nave(self, id, nave):
-        self.cursor.execute('UPDATE naves SET nombre_nave = ?, modelo = ?, capacidad_pasajeros = ? WHERE id = ?', (nave.nombre_nave, nave.modelo, nave.capacidad_pasajeros, id))
-        self.conexion.commit()
+        conn = self.db.conectar()
+        cursor = conn.cursor()
+        cursor.execute('UPDATE naves SET nombre_nave = ?, modelo = ?, capacidad_pasajeros = ? WHERE id = ?', (nave.nombre_nave, nave.modelo, nave.capacidad_pasajeros, id))
+        conn.commit()
+        conn.close()
 
     def query_delete_nave(self, id):
-        self.cursor.execute('DELETE FROM naves WHERE id = ?', (id))
-        self.conexion.commit()
+        conn = self.db.conectar()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM naves WHERE id = ?', (id))
+        conn.commit()
+        conn.close()
 
